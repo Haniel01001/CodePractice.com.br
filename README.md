2052aefdb22869b1cfdf188a3d6a2ec4d32c798d
Haniel <hanielnasiniak@gmail.com>
2026-09-15 19:51:20 +0000

    Adding prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CodePractice.com.br
+
 LetItHappend
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá tudo bem? meu nome e Haniel!</h1>;      
-};
+  return <h1>Olá tudo bem? meu nome e Haniel!</h1>;
+}
 
 export default Home;
